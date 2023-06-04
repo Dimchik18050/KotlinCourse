92911507283db5adf73a5126226864d1d34c83a3
Dmytro Khomenko <dimitriy.khomenko@ukr.net>
2023-06-04 16:12:41 +0300

    MyFirstApp: Working with the main function

--- MyFirstApp/src/main/kotlin/App.kt
+++ MyFirstApp/src/main/kotlin/App.kt
@@ -1,3 +1,30 @@
 fun main(args: Array<String>){
     println("Pow!")
+
+    // Commands
+    var x = 3
+    val name = "Cormoran"
+    x = x * 10
+    print("x is $x.")
+    // This is comment
+
+    // Loops
+    while (x > 20){
+        x = x - 1
+        print(" x is now $x.")
+    }
+    for (i in 1..100){
+        x = x + 1
+        print(" x is now $x.")
+    }
+
+    // Conditions
+    if (x == 20){
+        println(" x must be 20.")
+    } else {
+        println(" x isn't 20.")
+    }
+    if (name.equals("Cormoran")) {
+        println("$name Strike")
+    }
 }
