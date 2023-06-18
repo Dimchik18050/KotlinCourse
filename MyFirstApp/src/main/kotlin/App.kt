764550e4c742b4c274b0f9a9c48c137d251a10ed
Dmytro Khomenko <dimitriy.khomenko@ukr.net>
2023-06-18 19:13:09 +0300

    Working with loops. Loop While.

--- MyFirstApp/src/main/kotlin/App.kt
+++ MyFirstApp/src/main/kotlin/App.kt
@@ -1,30 +1,10 @@
 fun main(args: Array<String>){
-    println("Pow!")
-
-    // Commands
-    var x = 3
-    val name = "Cormoran"
-    x = x * 10
-    print("x is $x.")
-    // This is comment
-
-    // Loops
-    while (x > 20){
-        x = x - 1
-        print(" x is now $x.")
-    }
-    for (i in 1..100){
+    var x = 1
+    println("Before the loop. x = $x.")
+    while (x < 4) {
+        println("In the loop. x = $x.")
         x = x + 1
-        print(" x is now $x.")
     }
 
-    // Conditions
-    if (x == 20){
-        println(" x must be 20.")
-    } else {
-        println(" x isn't 20.")
-    }
-    if (name.equals("Cormoran")) {
-        println("$name Strike")
-    }
+    println("After the loop. x = $x.")
 }
